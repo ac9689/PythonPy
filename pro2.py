7fa1a342c1f11715e11faa288edfa3c6e605621c
ashwani9689@outlook.com <ashwani9689@outlook.com>
2025-05-01 21:38:18 +0530

    List and Dictonary with comments

--- pro2.py
+++ pro2.py
@@ -1,17 +1,20 @@
-my=[4,6,5,"helloworld",3.14]
+my=[4,6,5,"helloworld",3.14] # List with mixed data types
 del my[1]
 #my.extend(my[1:10])  # Example: Extending the list with a slice of itself
 my.insert(1, 2)  # Example: Inserting an element at index 1
 my[2]= 88
 print(my)
-my_dict = {
+my_dict = {   # Dictionary to store user information
     "name": "Alice",
     "age": 25,
     "city": "Delhi"
 }
 for key, value in my_dict.items():
     print(key, value)
-book = {
+    
+    
+    
+book = {         # Dictionary to store book information
     "title": "Python Basics",
     "author": "John Smith",
     "isbn": "978-1-23456-789-7",
@@ -32,5 +35,4 @@ book["shelf"] = "A3"
 print("\nBook Details:")
 for key, value in book.items():
     print(f"{key}: {value}")
-
 
