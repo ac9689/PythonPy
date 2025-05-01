3d40e124f9f1525867f061a664066f6a7ff94217
ashwani9689@outlook.com <ashwani9689@outlook.com>
2025-05-01 23:45:53 +0530

    check before Append

---
+++ pro3.py
@@ -0,0 +1,10 @@
+#check befor appending
+file = open('/home/ashwani/Desktop/wwe.txt', 'r')
+obj=file.read()
+print(obj)
+# Check if "busitantsinc" is not in the file content
+# Append "Busitantsinc" to the file if not present
+if "busitantsinc" not in obj:
+    file= open('/home/ashwani/Desktop/wwe.txt', 'a')
+    a = file.write("john\n")
+    print(a)
