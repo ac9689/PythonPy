41e35f717df7f5b18d31f3ccadc089bd4ed20749
ashwani9689@outlook.com <ashwani9689@outlook.com>
2025-05-02 21:19:59 +0530

    user input, control statements, loop

---
+++ pro6.py
@@ -0,0 +1,19 @@
+# user input
+name = input("enter the name : ")
+print("hello", name)
+
+fruits = ['apple', 'banana', 'cherry']
+for fruit in fruits:
+    print(fruit)
+    
+person = {'name': 'John', 'age': 30, 'city': 'New York'}
+for key, value in person.items():
+    print(f'{key}: {value}')
+    
+for i in range(5):
+    print(i)
+else:
+    print("Loop completed without a break.")
+    
+numbers = [i for i in range(10) if i % 2 == 0]
+print(numbers)
