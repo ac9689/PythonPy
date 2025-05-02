55335fb5f708fbeae844b0cc5cd1d1e549b15ecf
ashwani9689@outlook.com <ashwani9689@outlook.com>
2025-05-02 23:55:32 +0530

    check username and pwd using control statement

---
+++ pro9.py
@@ -0,0 +1,9 @@
+username = input('Enter Username : ')
+password = input('Enter Password : ')
+if username == "Ashwani968":
+    if password == "12345":
+        print('Welcome Ashwani Chauhan ')
+    else:
+        print('Incorrect password ')
+else:
+    print('user not found')
