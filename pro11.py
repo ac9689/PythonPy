60d6b54b144d28bf8dc450e7d94742d64a25537c
ashwani9689@outlook.com <ashwani9689@outlook.com>
2025-05-07 20:43:08 +0530

    Module file

---
+++ pro11.py
@@ -0,0 +1,12 @@
+tot=0
+for i in range(2, 11, 5):
+    tot += i
+    continue
+    print("sum is :", tot)
+    
+
+n = int(input("Enter a number: "))
+total = 0
+for j in range(1, n):
+    total += j
+print("Sum is:", total)
--- pro7.py
+++ pro7.py
@@ -1,5 +1,6 @@
-num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))  # check number is even or odd
 if num % 2 == 0:
     print(f"{num} is even")
 else:
-    print(f"{num}is odd")
+    print(f"{num} is odd")
+    
